d3603e63e8c87b3a3aef02a0519d0a91da5ffb03
russhenson <49965783+russhenson@users.noreply.github.com>
2021-04-24 12:21:48 +0800

    list view

--- app/src/main/java/com/mobdeve/hensonruss/androidchallenge1/MainActivity.java
+++ app/src/main/java/com/mobdeve/hensonruss/androidchallenge1/MainActivity.java
@@ -3,12 +3,29 @@ package com.mobdeve.hensonruss.androidchallenge1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView emailListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        emailListView = (ListView) findViewById(R.id.emailListView);
+
+        ArrayList<String> emailList = new ArrayList<>();
+
+        emailList.add("Email 1");
+        emailList.add("Email 2");
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,emailList);
+
+        emailListView.setAdapter(arrayAdapter);
     }
 }
