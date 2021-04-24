8f5cec3c4cea0c6d74b637a291d64065e6dca7d8
russhenson <49965783+russhenson@users.noreply.github.com>
2021-04-24 13:19:20 +0800

    list view trial

--- app/src/main/java/com/mobdeve/hensonruss/androidchallenge1/MainActivity.java
+++ app/src/main/java/com/mobdeve/hensonruss/androidchallenge1/MainActivity.java
@@ -1,10 +1,17 @@
 package com.mobdeve.hensonruss.androidchallenge1;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -20,13 +27,49 @@ public class MainActivity extends AppCompatActivity {
 
         emailListView = (ListView) findViewById(R.id.emailListView);
 
-        ArrayList<String> emailList = new ArrayList<>();
+        ArrayList<String> receiverList = new ArrayList<>();
+        ArrayList<String> subjectList = new ArrayList<>();
+        ArrayList<String> bodyList = new ArrayList<>();
 
-        emailList.add("Email 1");
-        emailList.add("Email 2");
+        receiverList.add("henson.rj10@gmail.com");
+        receiverList.add("steezymnl@gmail.com");
+        receiverList.add("henson.russ10@gmail.com");
+        subjectList.add("Question regarding Photoshop");
+        subjectList.add("Steezy design - April");
+        subjectList.add("Meeting set");
 
-        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,emailList);
+        EmailListAdapter adapter = new EmailListAdapter(this, receiverList, subjectList);
+        emailListView.setAdapter(adapter);
+    }
 
-        emailListView.setAdapter(arrayAdapter);
+    class EmailListAdapter extends ArrayAdapter<String> {
+
+        Context context;
+        ArrayList<String> rReceiver;
+        ArrayList<String> rSubject;
+
+        EmailListAdapter(Context c, ArrayList<String> receiver, ArrayList<String> subject) {
+            super(c, R.layout.email_layout, R.id.sentEmailReceiverTv, receiver);
+            this.context = c;
+            this.rReceiver = receiver;
+            this.rSubject = subject;
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            LayoutInflater layoutInflater = (LayoutInflater)getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View email = layoutInflater.inflate(R.layout.email_layout, parent, false);
+            TextView receiver = email.findViewById(R.id.sentEmailSubjectTv);
+            TextView subject = email.findViewById(R.id.sentEmailSubjectTv);
+
+            receiver.setText(rReceiver.get(position));
+            subject.setText(rSubject.get(position));
+
+            return email;
+        }
+
+
+
     }
 }
