975e03c4f42f2e42ab05de4eefbbefd5ac201e53
russhenson <49965783+russhenson@users.noreply.github.com>
2021-04-24 20:41:49 +0800

    updated the latest email views

--- app/src/main/java/com/mobdeve/hensonruss/androidchallenge1/EmailActivity.java
+++ app/src/main/java/com/mobdeve/hensonruss/androidchallenge1/EmailActivity.java
@@ -19,8 +19,8 @@ public class EmailActivity extends AppCompatActivity {
         getSupportActionBar().hide(); // hide title bar
         setContentView(R.layout.activity_email);
 
-        /*// Initialization of the views
-        this.senderTv = findViewById(R.id.emailSenderTv);
+        // Initialization of the views
+        this.senderTv = findViewById(R.id.senderTv);
         this.receiverTv = findViewById(R.id.emailReceiverTv);
         this.subjectTv = findViewById(R.id.emailSubjectTv);
         this.bodyTv = findViewById(R.id.emailBodyTv);
@@ -28,14 +28,14 @@ public class EmailActivity extends AppCompatActivity {
         // Get intent data
         Intent i = getIntent();
         String sender = "From: me";
-        String receiver;
-        String subject;
-        String body;
+        String receiver = i.getStringExtra(MainActivity.RECEIVER_TAG);
+        String subject = i.getStringExtra(MainActivity.SUBJECT_TAG);
+        String body = i.getStringExtra(MainActivity.BODY_TAG);
 
         // Set Text attributes of all views
         this.senderTv.setText(sender);
         this.receiverTv.setText(receiver);
         this.subjectTv.setText(subject);
-        this.bodyTv.setText(body);*/
+        this.bodyTv.setText(body);
     }
 }
